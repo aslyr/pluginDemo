1b957267ade5dfe277135ca0d4ad61454902a7ec
Think <aslyr2020@aliyun.com>
2019-09-23 23:25:58 +0800

    加入特效，blood，power

---
+++ src/main/kotlin/Attackable.kt
@@ -0,0 +1,6 @@
+/**
+ *@author:aslyr
+ *@date:create in 21:13 2019/9/23 2019
+ */
+interface Attackable {
+}
---
+++ src/main/kotlin/AutoMove.kt
@@ -0,0 +1,6 @@
+/**
+ *@author:aslyr
+ *@date:create in 16:21 2019/9/23 2019
+ */
+interface AutoMove {
+}
