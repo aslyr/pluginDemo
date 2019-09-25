8164f103a9d6c2adf446a0df86201fbcc74fbd3d
Think <aslyr2020@aliyun.com>
2019-09-25 08:16:52 +0800

    添加敌方坦克

---
+++ src/main/kotlin/AutoShot.kt
@@ -0,0 +1,6 @@
+/**
+ *@author:aslyr
+ *@date:create in 23:18 2019/9/24 2019
+ */
+interface AutoShot {
+}
---
+++ src/main/kotlin/EnemyTank.kt
@@ -0,0 +1,6 @@
+/**
+ *@author:aslyr
+ *@date:create in 21:38 2019/9/24 2019
+ */
+class EnemyTank {
+}
